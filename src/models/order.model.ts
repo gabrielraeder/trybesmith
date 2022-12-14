ef2dc6738861b4c1d92d61febd30e191abd2b579
gabrielraeder <gabrielraeder@outlook.com.br>
2022-12-14 20:20:28 -0300

    refatorado req4 - para usar JSON_ARRAYAGG

--- src/models/order.model.ts
+++ src/models/order.model.ts
@@ -1,6 +1,6 @@
 import { Pool, RowDataPacket, ResultSetHeader } from 'mysql2/promise';
-import { Order } from '../interfaces/order.interface';
-import { Product } from '../interfaces/product.interface';
+import { OrderProducts } from '../interfaces/order.interface';
+// import { Product } from '../interfaces/product.interface';
 import connection from './connection';
 
 export default class ProductModel {
@@ -10,18 +10,30 @@ export default class ProductModel {
     this.connection = connection;
   }
 
-  public async getAll(): Promise<Order[]> {
-    const [rows] = await this.connection
-      .execute<RowDataPacket[]>('SELECT * FROM Trybesmith.orders');
-    return rows as Order[];
-  }
+  // public async getAll(): Promise<Order[]> {
+  //   const [rows] = await this.connection
+  //     .execute<RowDataPacket[]>('SELECT * FROM Trybesmith.orders');
+  //   return rows as Order[];
+  // }
 
-  public async getByOrderId(id: number): Promise<Product[]> {
-    const [rows] = await this.connection.execute<RowDataPacket[]>(
-      'SELECT * FROM Trybesmith.products WHERE order_id=?',
-      [id],
+  // public async getByOrderId(id: number): Promise<Product[]> {
+  //   const [rows] = await this.connection.execute<RowDataPacket[]>(
+  //     'SELECT * FROM Trybesmith.products WHERE order_id=?',
+  //     [id],
+  //   );
+  //   return rows as Product[];
+  // }
+
+  public async getAll() {
+    const [rows] = await this.connection
+      .execute<RowDataPacket[]>(
+      `SELECT o.id, user_id as userId, JSON_ARRAYAGG(p.id) AS productsIds
+      FROM Trybesmith.orders AS o
+      INNER JOIN Trybesmith.products AS p 
+      ON o.id = p.order_id 
+      GROUP BY o.id;`,
     );
-    return rows as Product[];
+    return rows as OrderProducts[];
   }
 
   public async create(userId: number): Promise<number> {
--- src/services/order.service.ts
+++ src/services/order.service.ts
@@ -10,17 +10,21 @@ export default class ProductService {
   }
 
   public async getAll(): Promise<OrderProducts[]> {
-    const orders = await this.model.getAll();
-    const orderIds = orders.map(({ id }) => id);
-    const promises = orderIds.map((id) => this.model.getByOrderId(+id));
-    const products = await Promise.all(promises);
-    const orderProducts = orders.map((order, index) => ({
-      id: order.id,
-      userId: order.user_id,
-      productsIds: products[index].map((item) => item.id),
-    }));
-    return orderProducts;
+    const result = await this.model.getAll();
+    return result;
   }
+  // public async getAll(): Promise<OrderProducts[]> {
+  //   const orders = await this.model.getAll();
+  //   const orderIds = orders.map(({ id }) => id);
+  //   const promises = orderIds.map((id) => this.model.getByOrderId(+id));
+  //   const products = await Promise.all(promises);
+  //   const orderProducts = orders.map((order, index) => ({
+  //     id: order.id,
+  //     userId: order.user_id,
+  //     productsIds: products[index].map((item) => item.id),
+  //   }));
+  //   return orderProducts;
+  // }
 
   public async create(userId: number, productsIds: number[]) {
     const validate = productsIdsSchema.validate({ productsIds });
